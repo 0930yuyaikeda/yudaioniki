d1deeb27998e7a7f6e94e65fcf152f4167368ac1
0930yuyaikeda <0930arnaud@gmail.com>
2025-12-19 13:37:03 +0700

    TOPページを作成

---
+++ src/scripts/carousel.js
@@ -0,0 +1,100 @@
+const carouselElement = document.getElementById('carousel-example');
+
+const items = [
+    {
+        position: 0,
+        el: document.getElementById('carousel-item-1'),
+    },
+    {
+        position: 1,
+        el: document.getElementById('carousel-item-2'),
+    },
+    {
+        position: 2,
+        el: document.getElementById('carousel-item-3'),
+    },
+    {
+        position: 3,
+        el: document.getElementById('carousel-item-4'),
+    },
+    {
+        position: 4,
+        el: document.getElementById('carousel-item-5'),
+    },
+    {
+        position: 5,
+        el: document.getElementById('carousel-item-6'),
+    },
+];
+
+// options with default values
+const options = {
+    defaultPosition: 1,
+    interval: 3000,
+
+    indicators: {
+        activeClasses: 'bg-white dark:bg-gray-800',
+        inactiveClasses:
+            'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+        items: [
+            {
+                position: 0,
+                el: document.getElementById('carousel-indicator-1'),
+            },
+            {
+                position: 1,
+                el: document.getElementById('carousel-indicator-2'),
+            },
+            {
+                position: 2,
+                el: document.getElementById('carousel-indicator-3'),
+            },
+            {
+                position: 3,
+                el: document.getElementById('carousel-indicator-4'),
+            },
+            {
+                position: 4,
+                el: document.getElementById('carousel-indicator-5'),
+            },
+            {
+                position: 5,
+                el: document.getElementById('carousel-indicator-6'),
+            },
+        ],
+    },
+
+    // callback functions
+    onNext: () => {
+        console.log('next slider item is shown');
+    },
+    onPrev: () => {
+        console.log('previous slider item is shown');
+    },
+    onChange: () => {
+        console.log('new slider item has been shown');
+    },
+};
+
+// instance options object
+const instanceOptions = {
+    id: 'carousel-example',
+    override: true
+};
+
+const carousel = new Carousel(carouselElement, items, options, instanceOptions);
+
+// goes to the next (right) slide
+carousel.next();
+
+// goes to the previous (left) slide
+carousel.prev();
+
+// jumps to the 3rd position (position starts from 0)
+carousel.slideTo(2);
+
+// starts or resumes the carousel cycling (automated sliding)
+carousel.cycle();
+
+// pauses the cycling (automated sliding)
+carousel.pause();
---
+++ src/scripts/loading-script.js
@@ -0,0 +1,24 @@
+'use strict';
+// ***==========*** ↓↓  Loading event! ↓↓  ***==========***
+window.onload = function() {
+
+    setTimeout(() => {
+
+        // Get loading element
+        const loading = document.getElementById("loading");
+        const content = document.getElementById("content");
+        loading.classList.add("hidden");
+        content.classList.add("animate__animated");
+        content.classList.add("animate__fadeIn");
+        content.classList.remove("hidden");
+
+        // When end the loading, remove loading element
+        // There is [transition] in css so it need 1000
+        window.setTimeout(function(){
+            loading.remove();
+        }, 10);
+
+    }, 10);
+}
+
+
